1675f0360f889bb6e456e60226652490f8786d8b
Kadi Kraman <hellokadi@gmail.com>
2021-11-04 20:56:17 +0000

    Return partial data with graphcache

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
-import { createClient, Provider as UrqlProvider } from 'urql';
-import { StatusBar, Platform } from 'react-native';
+import {
+  createClient,
+  Provider as UrqlProvider,
+  dedupExchange,
+  fetchExchange,
+} from 'urql';
+import { Platform, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootNavigator } from './screens/Root.navigator';
+import { cacheExchange } from '@urql/exchange-graphcache';
+import schema from './graphql/graphql.schema.json';
 
 const client = createClient({
   url:
     Platform.OS === 'android'
       ? 'http://10.0.2.2:3000/graphql'
       : 'http://localhost:3000/graphql',
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      schema: schema as any,
+      resolvers: {
+        Query: {
+          story: (_, args) => ({ __typename: 'Story', id: args.id }),
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
 export const App: React.FC = () => {
